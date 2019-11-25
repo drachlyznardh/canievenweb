dac084bcc744037dde7d05d78f837759163cdf4d
Ivan Simonini <gods@roundhousecode.com>
2019-11-25 20:26:35 +0100

    Names and locations

--- public/album.js
+++ public/album.js
@@ -7,16 +7,28 @@ const loadAlbums = () => {
 	const albums = JSON.parse(request.responseText);
 
 	for (let album of albums) {
+
 		document.getElementById('albums').innerHTML = document.getElementById('albums').innerHTML + `
 			<div class="album">
+				<h3>${album.id} &ndash; ${album.title}</h3>
 				<img class="thumbnail" src="" />
 				<div class="description">
 					<strong>${album} photos</strong>
-					scattate da
-					${album.title}, ${album.title}
+					scattate da <a id="author-${album.id}">some author</a>, <a id="location-${album.id}">some place</a>
 				</div>
 			</div>
 		`
+
+		fetch("https://jsonplaceholder.typicode.com/users/" + album.userId + "/").then(response => response.json())
+			.then(json => {
+				var author = document.getElementById('author-' + album.id)
+				author.innerHTML = json.name
+				author.href = json.website
+
+				var userLocation = document.getElementById('location-' + album.id)
+				userLocation.innerHTML = json.address.city
+				userLocation.href = 'https://www.google.com/maps/@' + json.address.geo.lat + ',' + json.address.geo.lng + ',15z'
+		})
 	}
 }
 
