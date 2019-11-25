9b35e3c58d65be64eb1c1728522f42156c32368a
Ivan Simonini <gods@roundhousecode.com>
2019-11-25 20:52:14 +0100

    Better

--- public/album.js
+++ public/album.js
@@ -6,13 +6,12 @@ const loadAlbums = () => {
 
 				document.getElementById('albums').innerHTML = document.getElementById('albums').innerHTML + `
 					<div class="album">
-						<h3>${album.id} &ndash; ${album.title}</h3>
+						<img id="thumbnail-${album.id}" class="thumbnail" src="/node.svg"/>
 						<div class="description">
 							<span id="counter-${album.id}">Qualche</span> foto</strong><br/>
 							scattate da<br/>
 							<span class="glyphicon glyphicon-user"></span> <a id="author-${album.id}">qualcuno</a>, <span class="glyphicon glyphicon-map-marker"></span> <a id="location-${album.id}">da qualche parte</a>
 						</div>
-						<img id="thumbnail-${album.id}" class="thumbnail" src="/node.svg"/>
 					</div>
 				`
 
