4e29fa88db66ddca69f311d319d4275997b1905b
Ivan Simonini <gods@roundhousecode.com>
2019-11-25 20:50:25 +0100

    Progress

--- public/album.js
+++ public/album.js
@@ -1,35 +1,33 @@
 
 const loadAlbums = () => {
-	const request = new XMLHttpRequest();
-	request.open("GET", "https://jsonplaceholder.typicode.com/albums", false);
-	request.send();
+	fetch("https://jsonplaceholder.typicode.com/albums").then(response => response.json())
+		.then(albums => {
+			for (let album of albums) {
 
-	const albums = JSON.parse(request.responseText);
+				document.getElementById('albums').innerHTML = document.getElementById('albums').innerHTML + `
+					<div class="album">
+						<h3>${album.id} &ndash; ${album.title}</h3>
+						<div class="description">
+							<span id="counter-${album.id}">Qualche</span> foto</strong><br/>
+							scattate da<br/>
+							<span class="glyphicon glyphicon-user"></span> <a id="author-${album.id}">qualcuno</a>, <span class="glyphicon glyphicon-map-marker"></span> <a id="location-${album.id}">da qualche parte</a>
+						</div>
+						<img id="thumbnail-${album.id}" class="thumbnail" src="/node.svg"/>
+					</div>
+				`
 
-	for (let album of albums) {
+				fetch("https://jsonplaceholder.typicode.com/users/" + album.userId + "/").then(response => response.json())
+					.then(json => {
+						var author = document.getElementById('author-' + album.id)
+						author.innerHTML = json.name
+						author.href = json.website
 
-		document.getElementById('albums').innerHTML = document.getElementById('albums').innerHTML + `
-			<div class="album">
-				<h3>${album.id} &ndash; ${album.title}</h3>
-				<img class="thumbnail" src="" />
-				<div class="description">
-					<strong>${album} photos</strong>
-					scattate da <span class="glyphicon glyphicon-user"></span> <a id="author-${album.id}">some author</a>, <span class="glyphicon glyphicon-map-marker"></span> <a id="location-${album.id}">some place</a>
-				</div>
-			</div>
-		`
-
-		fetch("https://jsonplaceholder.typicode.com/users/" + album.userId + "/").then(response => response.json())
-			.then(json => {
-				var author = document.getElementById('author-' + album.id)
-				author.innerHTML = json.name
-				author.href = json.website
-
-				var userLocation = document.getElementById('location-' + album.id)
-				userLocation.innerHTML = json.address.city
-				userLocation.href = 'https://www.google.com/maps/@' + json.address.geo.lat + ',' + json.address.geo.lng + ',15z'
-		})
-	}
+						var userLocation = document.getElementById('location-' + album.id)
+						userLocation.innerHTML = json.address.city
+						userLocation.href = 'https://www.google.com/maps/@' + json.address.geo.lat + ',' + json.address.geo.lng + ',15z'
+				})
+			}
+	})
 }
 
 loadAlbums();
