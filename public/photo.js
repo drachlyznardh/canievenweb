12e1f848c320a8683b1a210086d85d588da77f69
Ivan Simonini <gods@roundhousecode.com>
2019-11-26 16:49:34 +0100

    This is it, for the logic

--- public/photo.js
+++ public/photo.js
@@ -1,4 +1,15 @@
 
+const loadPhoto = (id) => {
+
+	fetch('http://jsonplaceholder.typicode.com/photos?id=' + id).then(response => response.json()).then(json => {
+		photo = json[0]
+		document.getElementById('photo-img').src = photo.url
+		document.getElementById('photo-description').textContent = photo.title
+	})
+
+	$('#tabnav a[href=#photo]').tab('show')
+}
+
 const backToAlbum = () => {
 	$('#tabnav a[href="#album"]').tab('show')
 }
