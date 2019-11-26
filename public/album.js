508271720d48eaa5bed7df2327c8c6492eea34f4
Ivan Simonini <gods@roundhousecode.com>
2019-11-26 09:54:15 +0100

    WIP

--- public/album.js
+++ public/album.js
@@ -1,12 +1,18 @@
 
-modalAlbum = document.getElementById('album')
+// album = document.getElementById('album')
 
-function toggleAlbum() { modalAlbum.classList.toggle("show-modal"); }
-function windowOnClick(event) { if (event.target === modalAlbum) toggleAlbum(); }
+// function toggleAlbum() {
+// 	$('#modal-album').modal("toggle")
+// }
+// function windowOnClick(event) { if (event.target === album) toggleAlbum(); }
 
-function loadAlbum(id) {
-	modalAlbum.innerHTML = id
+function loadAlbum(id) => {
+	$('#modal-album').modal("show")
+
+	fetch('https://jsonplaceholder.typicode.com/albums/' + id).then(response => json()).then(json => {
+		$('#album-title').innerHTML = json.title
+	})
 }
 
-window.addEventListener("click", windowOnClick)
+// window.addEventListener("click", windowOnClick)
 
--- public/gallery.js
+++ public/gallery.js
@@ -1,40 +1,47 @@
 
+const loadAlbum = (id) => {
+	$('#album-title').innerHTML = id
+
+	fetch('https://jsonplaceholder.typicode.com/photos?albumId=' + id).then(response => response.json()).then(json => {
+		$('#album-title').innerHTML = json.title
+	})
+
+	$('#modal-album').modal("show")
+}
+
 const loadGallery = () => {
-	fetch("https://jsonplaceholder.typicode.com/albums").then(response => response.json())
-		.then(json => {
-			for (let album of json) {
+	fetch("https://jsonplaceholder.typicode.com/albums").then(response => response.json()).then(json => {
+		for (let album of json) {
 
-				document.getElementById('gallery').innerHTML = document.getElementById('gallery').innerHTML + `
-					<div class="gallery-album">
-						<a class="thumbnail" onclick="toggleAlbum();loadAlbum(${album.id})">
-							<img onclick="toggleAlbum()" id="thumbnail-${album.id}" src="/node.svg"/>
-						</a>
-						<div class="description">
-							<span id="counter-${album.id}">Qualche</span> foto</strong><br/>
-							scattate da<br/>
-							<span class="glyphicon glyphicon-user"></span> <a id="author-${album.id}">qualcuno</a>,<br/>
-							<span class="glyphicon glyphicon-map-marker"></span> <a id="location-${album.id}">da qualche parte</a>
-						</div>
+			document.getElementById('gallery').innerHTML = document.getElementById('gallery').innerHTML + `
+				<div class="gallery-album">
+					<a class="thumbnail" onclick="loadAlbum(${album.id})">
+						<img id="thumbnail-${album.id}" src="/node.svg"/>
+					</a>
+					<div class="description">
+						<span id="counter-${album.id}">Qualche</span> foto</strong><br/>
+						scattate da<br/>
+						<span class="glyphicon glyphicon-user"></span> <a id="author-${album.id}">qualcuno</a>,<br/>
+						<span class="glyphicon glyphicon-map-marker"></span> <a id="location-${album.id}">da qualche parte</a>
 					</div>
-				`
+				</div>
+			`
 
-				fetch("https://jsonplaceholder.typicode.com/users/" + album.userId + "/").then(response => response.json())
-					.then(json => {
-						var author = document.getElementById('author-' + album.id)
-						author.innerHTML = json.name
-						author.href = 'http://' + json.website
+			fetch("https://jsonplaceholder.typicode.com/users/" + album.userId + "/").then(response => response.json()).then(json => {
+				var author = document.getElementById('author-' + album.id)
+				author.innerHTML = json.name
+				author.href = 'http://' + json.website
 
-						var userLocation = document.getElementById('location-' + album.id)
-						userLocation.innerHTML = json.address.city
-						userLocation.href = 'https://www.google.com/maps/@' + json.address.geo.lat + ',' + json.address.geo.lng + ',15z'
-				})
+				var userLocation = document.getElementById('location-' + album.id)
+				userLocation.innerHTML = json.address.city
+				userLocation.href = 'https://www.google.com/maps/@' + json.address.geo.lat + ',' + json.address.geo.lng + ',15z'
+			})
 
-				fetch("http://jsonplaceholder.typicode.com/photos?albumId=" + album.id).then(response => response.json())
-					.then(json => {
-						document.getElementById('counter-' + album.id).innerHTML = json.length
-						document.getElementById('thumbnail-' + album.id).src = json[Math.floor(Math.random() * json.length)].thumbnailUrl
-				})
-			}
+			fetch("http://jsonplaceholder.typicode.com/photos?albumId=" + album.id).then(response => response.json()).then(json => {
+				document.getElementById('counter-' + album.id).innerHTML = json.length
+				document.getElementById('thumbnail-' + album.id).src = json[Math.floor(Math.random() * json.length)].thumbnailUrl
+			})
+		}
 	})
 }
 
