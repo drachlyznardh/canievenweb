353de835f8e6ef42ba22c15743f545ac07616657
Ivan Simonini <gods@roundhousecode.com>
2019-11-26 08:27:34 +0100

    Renaming

--- public/album.js
+++ public/album.js
@@ -1,44 +1,5 @@
 
-const loadAlbums = () => {
-	fetch("https://jsonplaceholder.typicode.com/albums").then(response => response.json())
-		.then(albums => {
-			for (let album of albums) {
-
-				document.getElementById('albums').innerHTML = document.getElementById('albums').innerHTML + `
-					<div class="album">
-						<a class="thumbnail" onclick="toggleAlbum();loadAlbum(${album.id})">
-							<img onclick="toggleAlbum()" id="thumbnail-${album.id}" src="/node.svg"/>
-						</a>
-						<div class="description">
-							<span id="counter-${album.id}">Qualche</span> foto</strong><br/>
-							scattate da<br/>
-							<span class="glyphicon glyphicon-user"></span> <a id="author-${album.id}">qualcuno</a>,<br/>
-							<span class="glyphicon glyphicon-map-marker"></span> <a id="location-${album.id}">da qualche parte</a>
-						</div>
-					</div>
-				`
-
-				fetch("https://jsonplaceholder.typicode.com/users/" + album.userId + "/").then(response => response.json())
-					.then(json => {
-						var author = document.getElementById('author-' + album.id)
-						author.innerHTML = json.name
-						author.href = 'http://' + json.website
-
-						var userLocation = document.getElementById('location-' + album.id)
-						userLocation.innerHTML = json.address.city
-						userLocation.href = 'https://www.google.com/maps/@' + json.address.geo.lat + ',' + json.address.geo.lng + ',15z'
-				})
-
-				fetch("http://jsonplaceholder.typicode.com/photos?albumId=" + album.id).then(response => response.json())
-					.then(json => {
-						document.getElementById('counter-' + album.id).innerHTML = json.length
-						document.getElementById('thumbnail-' + album.id).src = json[Math.floor(Math.random() * json.length)].thumbnailUrl
-				})
-			}
-	})
-}
-
-modalAlbum = document.getElementById('modal-album')
+modalAlbum = document.getElementById('album')
 
 function toggleAlbum() { modalAlbum.classList.toggle("show-modal"); }
 function windowOnClick(event) { if (event.target === modalAlbum) toggleAlbum(); }
@@ -48,6 +9,4 @@ function loadAlbum(id) {
 }
 
 window.addEventListener("click", windowOnClick)
-
-loadAlbums();
 
