8e3c926d5c4da0c25137a04f1a373c4e4b52a8e0
Ivan Simonini <gods@roundhousecode.com>
2019-11-26 17:04:33 +0100

    Do not mix HTTPS with HTTP

--- public/photo.js
+++ public/photo.js
@@ -1,7 +1,7 @@
 
 const loadPhoto = (id) => {
 
-	fetch('http://jsonplaceholder.typicode.com/photos?id=' + id).then(response => response.json()).then(json => {
+	fetch('https://jsonplaceholder.typicode.com/photos?id=' + id).then(response => response.json()).then(json => {
 		photo = json[0]
 		document.getElementById('photo-img').src = photo.url
 		document.getElementById('photo-description').textContent = photo.title
